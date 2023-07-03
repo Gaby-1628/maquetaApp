d8d95d75da4848b60d5dc19762aef0fc2ab482f5
Gabriela Marisol Rodriguez Dos Santos <gabriela.rodriguez@outit.com.ar>
2023-07-03 13:35:04 -0300

    Se agrego seccion de cambio de contraseña

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 const singIn = document.querySelector('#sing-in')
 const singUp = document.querySelector('#sing-up')
+const password = document.querySelector('#password')
 
 singUp.style.display = 'none'
+password.style.display = 'none'
 
 function showHide(login) {
     if (login == 'singUp') {
         singIn.style.display = 'none'
+        password.style.display = 'none'
         singUp.style.display = 'flex'
     }
     else if (login == 'singIn') {
         singIn.style.display = 'flex'
+        password.style.display = 'none'
+        singUp.style.display = 'none'
+    }
+    else if (login == 'password') {
+        singIn.style.display = 'none'
+        password.style.display = 'flex'
         singUp.style.display = 'none'
     }
 }
